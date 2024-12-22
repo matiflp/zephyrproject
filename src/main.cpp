5971450b9d03c6daaea042aa4b47ddb0271f93f3
Matias Lopez <matias.lopez.artes@gmail.com>
2024-12-22 11:26:16 -0300

    fix hometask 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(void)
 {
-    cout << "Running on " << CONFIG_BOARD << " host board\n";
+    string board = string(CONFIG_BOARD);
+
+    if (board == "native_posix") {
+        cout << "The application is running on the native_posix_64 host board\n";
+    } else {
+        cout << "Running on " << CONFIG_BOARD << " host board.\n";
+    }
+
     return 0;
 }
