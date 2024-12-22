5de46d0e77f7452d285cb4133e2eaca27e3a14e3
Matias Lopez <matias.lopez.artes@gmail.com>
2024-12-22 11:10:20 -0300

    move project to the root

--- app/src/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include <zephyr/sys/printk.h>
-
-int main(void)
-{
-    printk("Running on %s\n host board\n", CONFIG_BOARD);
-    return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    cout << "Running on " << CONFIG_BOARD << " host board\n";
+    return 0;
+}
