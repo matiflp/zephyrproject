68c2230f0100b08bf3cab4969e471bdc7e9a3f67
Matias Lopez <matias.lopez.artes@gmail.com>
2024-12-23 15:54:53 -0300

    fix cpp cmake configuration error and add constexpr

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,9 @@ LOG_MODULE_REGISTER(my_log_module, CONFIG_ZEPHYR_COURSE_LOG_LEVEL);
 
 int main(void)
 {
-    string board = string(CONFIG_BOARD);
+    constexpr bool is_native_posix = (CONFIG_BOARD == "native_posix_64");
 
-    if (board == "native_posix") {
+    if (is_native_posix) {
         cout << "The application is running on the native_posix_64 host board\n";
     } else {
         cout << "Running on " << CONFIG_BOARD << " host board.\n";
